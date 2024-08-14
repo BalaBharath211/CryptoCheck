aeea4ac8b85f9af3c1dcbed8605d5e05b383cb3b
Bharath <veerabalabharath211@gmail.com>
2024-08-14 12:08:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.blockchain.com/v3/exchange/tickers";
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/",(req, res) => {
+    res.render('index.ejs');
+    
+});
+
+app.post('/check-price', async (req, res) => {
+    function calculate24hChange(currentPrice, price24hAgo) {
+        if (!price24hAgo || price24hAgo === 0) {
+            return 0; 
+        }
+        
+        const change = ((currentPrice - price24hAgo) / price24hAgo) * 100;
+        return change.toFixed(2); 
+    }
+    function calculateMarketCap(currentPrice, circulatingSupply) {
+        return currentPrice * circulatingSupply;
+    }
+    const cryptoSymbol = req.body.cryptoSymbol.toUpperCase(); 
+    const apiKey = '73c4c929-1b75-4b2f-a755-57ec3c3b54de'; 
+
+    try {
+        const response = await axios.get('https://api.blockchain.com/v3/exchange/tickers', {
+            headers: {
+                'X-API-Token': apiKey
+            }
+        });
+
+        const ticker = response.data.find(ticker => ticker.symbol === `${cryptoSymbol}`);
+        
+        if (ticker) {
+            const currentPrice = ticker.last_trade_price;
+            const price24hAgo = ticker.price_24h || ticker.open_24h; 
+            const circulatingSupply = 18000000; 
+            
+            const change24h = calculate24hChange(currentPrice, price24hAgo);
+
+            const marketCap = calculateMarketCap(currentPrice, circulatingSupply);
+    
+            res.render("index.ejs",{content :`$${ticker.last_trade_price}`, 
+                                    Two4:`${ticker.price_24h}`,
+                                    volume:`${ticker.volume_24h}`,
+                                    cryptoName:`${ticker.symbol}`,
+                                    percentage:`${change24h}%` ,
+                                    MarketCap: `$${marketCap.toLocaleString()} USD`, 
+                                                      data:response.data});
+        }   
+        else {
+            res.send(`Ticker for ${cryptoSymbol} not found.`);
+        }
+    } catch (error) {
+        console.error('Error fetching cryptocurrency price:', error);
+        res.send('An error occurred while fetching the price.');
+    }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
